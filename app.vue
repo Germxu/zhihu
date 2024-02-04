161999562e7bde49616dc5ec65ce677293e48dc9
Finn <Germxu@users.noreply.github.com>
2024-02-04 08:21:25 +0000

    color-theme config

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
 <template>
   <NuxtPage />
 </template>
+
+<script setup lang="ts">
+const {query} = useRoute();
+
+useHead({
+  htmlAttrs: {
+    "data-theme" : query.theme || process.client && window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+  }
+})
+</script>
